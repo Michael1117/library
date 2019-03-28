12af50cc74cfe672fa83cc35234ef352f78f51dd
Michael1117 <1322720377@qq.com>
2019-03-28 09:22:03 +0800

    target

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # library
 webpack_study
+
+
+## libraryTarget 可以是 global , window ,this, umd
+
+## library可以是library ， root
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
+const path = require('path')
+
 module.exports = {
-    
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'library.js',
+        library: 'library',  // 全局变量里加入一个 library
+        libraryTarget: "umd"  // umd cmd common.js
+    }
 }
